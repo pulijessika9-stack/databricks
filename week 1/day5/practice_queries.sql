9246342801f9cf19360fbdde73295a7fc459ca26
pulijessika9-stack <pulijessika9@gmail.com>
2026-07-08 19:12:33 +0530

    Create practice_queries.sql

---
+++ week 1/day5/practice_queries.sql
@@ -0,0 +1,20 @@
+*IN AND NOT IN*
+Find employees working in ('Hyderabad', 'Mumbai').
+Find employees whose department IN ('IT', 'Finance').
+Find employees whose city NOT IN ('Chennai', 'Pune').
+Find employees whose salary IN (45000, 75000, 91000).
+Find employees whose department NOT IN ('HR', 'Sales').
+
+*BETWEEN*
+Find employees with salary BETWEEN 50000 AND 80000.
+Find employees with experience BETWEEN 3 AND 6.
+Find employees whose emp_id BETWEEN 105 AND 112.
+Find employees with salary NOT BETWEEN 40000 AND 60000.
+Find employees with experience BETWEEN 2 AND 4.
+
+*LIKE OPERATOR*
+Find employees whose names start with 'R'.
+Find employees whose names end with 'a'.
+Find employees whose names contain 'v'.
+Find employees whose city starts with 'B'.
+Find employees whose department ends with 's'.

87229a6747912e8c9e8d7cb053c08d9a7ed6fa9b
pulijessika9-stack <pulijessika9@gmail.com>
2026-07-08 19:04:41 +0530

    Create practice_queries.sql

---
+++ week 1/day4/practice_queries.sql
@@ -0,0 +1,27 @@
+*TOP*
+Display top 5 highest paid employees.
+Display top 3 employees with highest experience.
+Display top 2 salaries from Finance department.
+Display top 4 employees from Hyderabad.
+Display top 1 highest salary employee.
+
+*DISTINCT*
+Display distinct department names.
+Display distinct city names.
+Display distinct salary values.
+Display distinct combinations of department and city.
+Display distinct experience values.
+
+*COMPARISON OPERATORS*
+Find employees with salary >= 80000.
+Find employees with experience <= 3.
+Find employees whose salary <> 45000.
+Find employees with salary < 50000.
+Find employees with experience > 5.
+
+*LOGICAL OPERATORS*
+Find employees from IT department AND salary greater than 70000.
+Find employees from Hyderabad OR Bangalore.
+Find employees from HR department AND experience less than 3.
+Find employees with salary greater than 60000 OR experience greater than 6.
+Find employees NOT from Sales department.

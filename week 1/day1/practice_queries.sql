201882868b77ad16a414ad4071be4a0c2e71a9aa
pulijessika9-stack <pulijessika9@gmail.com>
2026-07-08 18:39:24 +0530

    Create practice_queries.sql

---
+++ week 1/day1/practice_queries.sql
@@ -0,0 +1,19 @@
+select * from Employee
+  
+select name,salary from Employee
+
+select * from Employee  where age>30
+
+select name from Department
+
+select * from Employee where department_id=1
+
+select * from Employee where name like 'J%'
+
+select * from Employee where name like '%e'
+
+select * from Employee where name like '%a%'
+
+select * from Employee where length(name)=9
+
+select * from Employee where name like '_o%'
